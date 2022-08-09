0cb04723689d0522fc465f72b61118e54481af47
arzuna-dev <71864013+arzuna-dev@users.noreply.github.com>
2022-08-09 08:16:33 +0545

    images apps added

--- bookmark/settings.py
+++ bookmark/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'account.apps.AccountConfig',
-    'images.app.ImagesConfig',
+    'images.apps.ImagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
