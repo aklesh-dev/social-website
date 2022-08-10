e06e3e43a8c3bdc0b42775ab8a07e125ad44bef6
arzuna-dev <71864013+arzuna-dev@users.noreply.github.com>
2022-08-10 09:37:27 +0545

    build bookmarklet with jQuery

---
+++ images/templates/bookmarklet_luncher.js
@@ -0,0 +1,11 @@
+// bookmarklet luncher script
+(function(){
+    if (window.myBookmarklet !== undefinded){
+        myBookmarklet();
+    }
+    else{
+        document.body.appendChild(document.createElement('script')).
+        src = 'https://127.0.0.1:8000/static/js/bookmarklet.js?r='+Math.
+        floor(Math.random()*99999999999999999999);
+    }
+})();
